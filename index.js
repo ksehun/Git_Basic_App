51cde4e262a7b6d2cba7cf879c0ac3af40df68dc
ksh <mirsuro123@naver.com>
2023-11-08 15:41:36 +0900

    add함수수정 및 minus함수추가

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 /*
     index.html에서 사용하는 js 파일입니다.
 */
-function add(n1, n2) {
+function add(n1, n2, n3=10) {
+    if (n3 !== undefined) {
+        return n1 + n2 + n3;
+    }
     return n1 + n2;
+}
+
+function minus(n1, n2) {
+    if (n1 > n2) {
+        return n1 - n2;
+    } else {
+        return n2 - n1;
+    }
 }
