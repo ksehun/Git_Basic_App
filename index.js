fb24f0fa68e5de66985aa70ee0756abfc2bae727
ksh <mirsuro123@naver.com>
2023-11-08 15:44:37 +0900

    add함수수정

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 /*
     index.html에서 사용하는 js 파일입니다.
 */
-function add(n1, n2) {
+function add(n1, n2, msg) {
+    if (msg !== undefined) {
+        var result = n1 + n2;
+        return msg + result;
+    }
     return n1 + n2;
 }
